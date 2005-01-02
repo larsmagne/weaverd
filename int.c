6a6ed89771aaec37b2c522b606b530331544f74f
larsi <larsi>
2005-01-02 01:19:19 +0000

    Conf file and skip, etc.

--- int.c
+++ int.c
@@ -26,18 +26,23 @@ struct option long_options[] = {
   {"help", 0, 0, 'h'},
   {"user", 1, 0, 'u'},
   {"recursive", 0, 0, 'r'},
+  {"conf", 0, 0, 'c'},
+  {"skip", 0, 0, 'S'},
   {0, 0, 0, 0}
 };
 
 static int output_thread = 0;
 static int input_spool = 0;
+static int input_conf = 0;
+static char *lock_user = NULL;
+static char *skip_until_group = NULL;
 
 void closedown(int i);
 
 int parse_args(int argc, char **argv) {
   int option_index = 0, c;
   while (1) {
-    c = getopt_long(argc, argv, "htrs:i:u:", long_options, &option_index);
+    c = getopt_long(argc, argv, "htrcs:i:u:S:", long_options, &option_index);
     if (c == -1)
       break;
 
@@ -51,7 +56,11 @@ int parse_args(int argc, char **argv) {
       break;
       
     case 'u':
-      lock_and_uid(optarg);
+      lock_user = optarg;
+      break;
+      
+    case 'S':
+      skip_until_group = optarg;
       break;
       
     case 't':
@@ -60,6 +69,10 @@ int parse_args(int argc, char **argv) {
       
     case 'r':
       input_spool = 1;
+      break;
+      
+    case 'c':
+      input_conf = 1;
       break;
       
     case 'h':
@@ -77,7 +90,10 @@ int parse_args(int argc, char **argv) {
 #define MAX_FILE_NAME 10000
 
 static int commands = 0;
-static time_t start_time;
+static time_t start_time = 0;
+
+static char *ignore_until = "/mirror/var/spool/news/articles/gmane/linux/skolelinux/cvs";
+static int ignoring_p = 0;
 
 void input_directory(const char* dir_name) {
   time_t now;
@@ -86,6 +102,7 @@ void input_directory(const char* dir_name) {
   struct dirent *dp;
   char file_name[MAX_FILE_NAME];
   struct stat stat_buf;
+  int prohibited_p = 0;
 
   printf("%s\n", dir_name); 
 
@@ -105,23 +122,92 @@ void input_directory(const char* dir_name) {
 	break;
       }
     
-      if (S_ISDIR(stat_buf.st_mode)) 
-	input_directory(file_name);
-      else if (is_number(dp->d_name)) {
-	thread_file(file_name);
+      if (S_ISDIR(stat_buf.st_mode)) { 
+	if (! strcmp(file_name, ignore_until))
+	  ignoring_p = 0;
+	 if (ignoring_p == 0 || stat_buf.st_nlink > 2)
+	   input_directory(file_name);
+      } else if (ignoring_p == 0 && is_number(dp->d_name) 
+		 && prohibited_p == 0) {
+	prohibited_p = thread_file(file_name);
 	if (!(commands++ % 10000)) {
 	  time(&now);
-	  elapsed = now-start_time;
-	  printf("    %d files (%d/s, %d seconds)\n",
+	  elapsed = now - start_time;
+	  printf("    %d files (%d/s, last %d seconds)\n",
 		 commands - 1, 
 		 (elapsed?
-		  (int)(commands / elapsed):
-		  0),
+		  (int)(10000 / elapsed):
+		  0), 
 		 elapsed);
+	  start_time = now;
 	  printf("    %d bytes string storage per file\n",
 		 next_string / commands);
 	  mem_usage();
 	}
+      }
+    }
+  }
+  closedir(dirp);
+}
+
+char *get_group_directory(const char *group) {
+  static char file_name[1024];
+  char *f = file_name + strlen(news_spool);
+  
+  snprintf(file_name, 1024, "%s%s", news_spool, group);
+  
+  while (*f != 0) {
+    if (*f == '.')
+      *f = '/';
+    f++;
+  }
+
+  return file_name;
+}
+
+void input_group(const char* group_name) {
+  time_t now;
+  int elapsed;
+  DIR *dirp;
+  struct dirent *dp;
+  char file_name[MAX_FILE_NAME];
+  struct stat stat_buf;
+  int prohibited_p = 0;
+  char *dir_name = get_group_directory(group_name);
+
+  printf("%s\n", dir_name); 
+
+  if ((dirp = opendir(dir_name)) == NULL)
+    return;
+    
+  while ((dp = readdir(dirp)) != NULL) {
+
+    snprintf(file_name, sizeof(file_name), "%s/%s", dir_name,
+	     dp->d_name);
+
+    if (stat(file_name, &stat_buf) == -1) {
+      perror("tokenizer");
+      break;
+    }
+    
+    if (! S_ISDIR(stat_buf.st_mode) &&
+	ignoring_p == 0 &&
+	prohibited_p == 0 &&
+	is_number(dp->d_name)) {
+      prohibited_p = thread_file(file_name);
+      if (!(commands++ % 10000)) {
+	time(&now);
+	elapsed = now - start_time;
+	printf("    %d files (%d/s, last %d seconds)\n",
+	       commands - 1, 
+	       (elapsed?
+		(int)(10000 / elapsed):
+		0), 
+	       elapsed);
+	start_time = now;
+	printf("    %d bytes string storage per file\n",
+	       next_string / commands);
+	mem_usage();
       }
     }
   }
@@ -134,6 +220,8 @@ int main(int argc, char **argv)
   struct stat stat_buf;
   char *spool;
   char *file;
+  int i;
+  int found = 1;
 
   dirn = parse_args(argc, argv);
   
@@ -147,6 +235,10 @@ int main(int argc, char **argv)
     exit(1);
   }
 
+  if (input_conf) {
+    inhibit_thread_flattening = 1;
+  }
+
   /* Initialize key/data structures. */
   init();
   time(&start_time);
@@ -156,15 +248,42 @@ int main(int argc, char **argv)
     exit(0);
   } 
 
+  if (lock_user != NULL)
+    lock_and_uid(lock_user);
+
   if (input_spool) {
     spool = cmalloc(strlen(news_spool) + 1);
     memcpy(spool, news_spool, strlen(news_spool));
     inhibit_thread_flattening = 1;
     inhibit_file_writes = 0;
     *(spool+strlen(news_spool)-1) = 0;
-    //input_directory("/mirror/var/spool/news/articles/gmane/os/freebsd/current");
+    //input_directory("/mirror/var/spool/news/articles/gmane/comp/lib/glibc/bugs");
+    //input_directory("/mirror/var/spool/news/articles/gmane/comp/gnu/stow/bugs");
+    //input_directory("/mirror/var/spool/news/articles/gmane/linux");
     input_directory(spool);
     flush();
+    clean_up();
+    clean_up_hash();
+    printf("Total files: %d, total nodes: %d\n", commands, current_node);
+    exit(0);
+  } 
+
+  if (input_conf) {
+    if (skip_until_group != NULL)
+      found = 0;
+
+    inhibit_thread_flattening = 1;
+    inhibit_file_writes = 0;
+    for (i = 1; i < num_groups; i++) {
+      if (found == 0 && !strcmp(get_string(groups[i].group_name), 
+				skip_until_group))
+	found = 1;
+      if (found == 1)
+	input_group(get_string(groups[i].group_name));
+    }
+    flush();
+    clean_up();
+    clean_up_hash();
     printf("Total files: %d, total nodes: %d\n", commands, current_node);
     exit(0);
   } 
@@ -184,6 +303,7 @@ int main(int argc, char **argv)
 
   flush();
   clean_up();
+  clean_up_hash();
 
   exit(0);
 }
