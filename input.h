07a1e8eb8d7f9fb06686621c8048b21484956fb6
larsi <larsi>
2005-01-02 01:19:01 +0000

    Return value.

--- input.h
+++ input.h
@@ -1,7 +1,7 @@
 #ifndef THREAD_H
 #define THREAD_H
 
-void thread_file(const char *file_name);
+int thread_file(const char *file_name);
 void lock_and_uid(char *user);
 void read_conf_file(void);
 void wash_string(char *string);
