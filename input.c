5c1380ece0c7cfddc1e721387cbdee6c7257df93
larsi <larsi>
2005-01-02 01:18:42 +0000

    Lots of new stuff.  Eg., gathering loose threads.

--- input.c
+++ input.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#define  __USE_XOPEN2K
 #include <fcntl.h>
 #include <getopt.h>
 #include <dirent.h>
@@ -11,6 +12,9 @@
 #include <gmime/gmime.h>
 #include <pwd.h>
 #include <sys/types.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
 
 #include "weaver.h"
 #include "../mdb/util.h"
@@ -25,6 +29,7 @@ typedef struct {
   char author[MAX_STRING_SIZE];
   time_t date;
   int ignorep;
+  int likely_response;
 } parsed_article;
 
 void wash_string(char *string) {
@@ -37,7 +42,34 @@ void wash_string(char *string) {
   }
 }
 
+int quoted_in_body_p (const char *file_name) {
+  int file;
+  char buf[4096];
+  int read_size;
+  int found = 0;
+
+  bzero(buf, sizeof(buf));
+
+  if ((file = open(file_name, O_RDONLY|O_STREAMING)) == -1) {
+    return 0;
+  }
+
+  while ((read_size = read(file, &buf, sizeof(buf) - 1)) > 0) {
+    buf[sizeof(buf)] = 0;
+    if (strstr(buf, "\n>")) {
+      found = 1;
+      break;
+    }
+  }
+
+  close(file);
+  return found;
+}
+
 static parsed_article pa;
+
+GMimeMessage *
+g_mime_parser_construct_message_headers_only (GMimeStream *stream);
 
 parsed_article *parse_file(const char *file_name) {
   GMimeStream *stream;
@@ -59,10 +91,12 @@ parsed_article *parse_file(const char *file_name) {
   }
 
   stream = g_mime_stream_fs_new(file);
-  msg = g_mime_parser_construct_message(stream);
+  msg = g_mime_parser_construct_message_headers_only(stream);
   g_mime_stream_unref(stream);
 
   if (msg != 0) {
+    pa.likely_response = 0;
+
     author = g_mime_message_get_header(msg, "From");
     subject = g_mime_message_get_subject(msg);
     message_id = g_mime_message_get_message_id(msg);
@@ -101,6 +135,8 @@ parsed_article *parse_file(const char *file_name) {
 	*pa.author = 0;
       }
 
+      /* Remove all leading spaces, and all leading instances of 
+	 Re: from the subjects. */
       while (! washed_subject) {
 	washed_subject = 1;
 	while (*subject == ' ') {
@@ -113,6 +149,7 @@ parsed_article *parse_file(const char *file_name) {
 	    subject == strstr(subject, "RE:")) {
 	  subject += 3;
 	  washed_subject = 0;
+	  pa.likely_response = 1;
 	}
       }
 
@@ -130,9 +167,26 @@ parsed_article *parse_file(const char *file_name) {
 	  strlen(original_message_id) > 0 &&
 	  strstr(message_id, "gmane.org"))
 	strncpy(pa.message_id, original_message_id, MAX_STRING_SIZE-1);
-      else
+      else {
+	char *suffix;
 	strncpy(pa.message_id, message_id, MAX_STRING_SIZE-1);
+	*(pa.message_id+MAX_STRING_SIZE) = 0;
+	if ((suffix = strstr(pa.message_id, "__")) != NULL) {
+	  char *at;
+	  if ((at = strchr(suffix, '@'))) {
+	    while (*at != 0)
+	      *suffix++ = *at++;
+	  }
+	}
+      }
 
+      if (references == NULL) {
+	if (g_mime_message_get_header(msg, "in-reply-to"))
+	  pa.likely_response = 1;
+	if (quoted_in_body_p(file_name))
+	  pa.likely_response = 1;
+      }
+    
       if (references == NULL)
 	*(pa.parent_message_id) = 0;
       else
@@ -224,7 +278,7 @@ int path_to_article_wspec(const char *file_name, char *group, int *article) {
   return 1;
 }
 
-void thread_file(const char *file_name) {
+int thread_file(const char *file_name) {
   parsed_article *pa;
   node *tnode, *prev_node;
   char group_name[MAX_STRING_SIZE];
@@ -234,15 +288,15 @@ void thread_file(const char *file_name) {
   if (path_to_article_wspec(file_name, group_name, &article)) {
     g = get_group(group_name);
     if (prohibited_group_p(g)) 
-      return;
+      return 1;
 
     pa = parse_file(file_name);
 
     if (pa == NULL)
-      return;
+      return 0;
 
     if (pa->ignorep == 1)
-      return;
+      return 0;
 
 #ifdef DEBUG
     printf("%s %s %s %s\n", pa->author, pa->subject, pa->message_id,
@@ -280,17 +334,22 @@ void thread_file(const char *file_name) {
 	tnode->parent = get_parent(pa->parent_message_id, group_id);
 #if 0
 	if (nodes[tnode->parent].subject == 0) {
-	  printf("Not found '%s' '%s'\n", pa->message_id, pa->parent_message_id);
+	  printf("Not found '%s' '%s'\n", pa->message_id, 
+		 pa->parent_message_id);
 	} else {
-	  printf("    Found '%s' '%s'\n", pa->message_id, pa->parent_message_id);
+	  printf("    Found '%s' '%s'\n", pa->message_id,
+		 pa->parent_message_id);
 	}
 #endif
+      } else if (pa->likely_response) {
+	tnode->parent = get_parent_by_subject(pa->subject, group_id, pa->date);
       }
     
       /* Set next_instance in previous instances to point to us. */
       if (prev_instance) {
 	prev_node = &nodes[prev_instance];
 	prev_node->next_instance = id;
+	write_node(prev_node);
       }
 
       tnode->subject = enter_string_storage(pa->subject);
@@ -304,18 +363,51 @@ void thread_file(const char *file_name) {
   } else {
     printf("Can't find an article spec for %s\n", file_name);
   }
-  return;
+  return 0;
+}
+
+void mmlock(void *area, int size) {
+  if (mlock(area, size) == -1) {
+    perror("weaver mlock");
+    exit(1);
+  }
 }
 
 void lock_and_uid(char *user) {
   struct passwd *pw;
+  struct rlimit rlim;
 
   pw = getpwnam(user);
-  
-  if (mlockall(MCL_FUTURE) == -1) {
-    perror("we-index");
+
+  /*
+  bzero(&rlim, sizeof(rlim));
+  if (getrlimit(RLIMIT_MEMLOCK, &rlim) == -1) {
+    perror("weaver");
+  }
+  printf("Limit for locking is %ld:%ld (%ld)\n", rlim.rlim_cur, rlim.rlim_max,
+	 RLIM_INFINITY);
+  rlim.rlim_cur = 2000000000;
+  rlim.rlim_max = 2000000000;
+  if (setrlimit(RLIMIT_MEMLOCK, &rlim) == -1) {
+    perror("weaver");
+  }
+
+  if (mlockall(MCL_CURRENT) == -1) {
+    perror("weaver");
     exit(1);
   }
+  */
+
+  if (mlockall(MCL_CURRENT) == -1) {
+    perror("weaver");
+    exit(1);
+  }
+
+  /*
+  mmlock(groups, MAX_GROUPS * sizeof(group));
+  mmlock(string_storage, string_storage_length);
+  mmlock(nodes, nodes_length * sizeof(nodes));
+  */
 
   setuid(pw->pw_uid);
   setgid(pw->pw_gid);
@@ -324,7 +416,7 @@ void lock_and_uid(char *user) {
 void read_conf_file(void) {
   FILE *conf = fopen("/etc/gmane.conf", "r");
   char buf[8192];
-  char *string, *group_name, *description;
+  char *string, *group_name, *description, *external_name, *other_names;
   int i = 0;
   group *g;
 
@@ -336,19 +428,36 @@ void read_conf_file(void) {
       i = 0;
       group_name = NULL;
       description = NULL;
-      strtok(buf, ":");
-      while ((string = strtok(NULL, ":")) != NULL &&
-	     i < 4) {
+      external_name = NULL;
+      other_names = NULL;
+      strtok(buf, ":\n");
+      while ((string = strtok(NULL, ":\n")) != NULL) {
 	i++;
 	if (i == 1)
 	  group_name = string;
 	else if (i == 3)
 	  description = string;
+	else if (i > 5 && strstr(string, "external=") == string)
+	  external_name = string + strlen("external=");
+	else if (i > 5 && strstr(string, "other-names=") == string)
+	  other_names = string + strlen("other-names=");
       }
 
       if (group_name && description) {
 	g = get_group(group_name);
 	g->group_description = enter_string_storage(description);
+	if (external_name) {
+	  g->external_group_name = enter_string_storage(external_name);
+	  enter_external_to_internal_group_name_map(external_name, group_name);
+	}
+	if (other_names) {
+	  string = strtok(other_names, " ");
+	  if (string) {
+	    do {
+	      enter_external_to_internal_group_name_map(string, group_name);
+	    } while ((string = strtok(NULL, " ")) != NULL);
+	  }
+	}
       }
     }
   }
