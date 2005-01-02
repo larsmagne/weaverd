2b3b88e7c608ae730d257b190d5b427ce05e1ae7
larsi <larsi>
2005-01-02 01:17:12 +0000

    External group names, etc.

--- hash.h
+++ hash.h
@@ -16,7 +16,15 @@ void newgroup (FILE *output, char *group_name, char **description, int words);
 int prohibited_group_p(group *g);
 void rmgroup(FILE *output, char *group_name);
 node *get_node_any(const char *message_id, unsigned int group_id);
+char *get_group_name(group *g);
+void clean_up_hash(void);
+char *external_group_name(group *g);
+char *internal_group_name(const char *external);
+void enter_external_to_internal_group_name_map(const char *external, 
+					       const char *internal);
 
 extern int next_string;
+extern char *string_storage;
+extern int string_storage_length;
 
 #endif
