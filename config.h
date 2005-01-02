aa0016fd8784203a9593fb5f88990693adc00429
larsi <larsi>
2005-01-02 01:14:45 +0000

    More conf.

--- config.h
+++ config.h
@@ -9,13 +9,18 @@
 #define NODE_FILE "nodes.db"
 #define STRING_STORAGE_FILE "strings.db"
 #define MAX_STRING_SIZE 10240
-#define MAX_NODES 20000000
+#define MAX_NODES 25000000
 #define NEWS_SPOOL "/mirror/var/spool/news/articles/"
+#define INDEX_DIR "/index/weave"
 
-#define INITIAL_STRING_STORAGE_LENGTH (1024 * 1024 * 700)
-#define STRING_STORAGE_TABLE_LENGTH (1024 * 1024 * 32)
+#define INITIAL_STRING_STORAGE_LENGTH (1024 * 1024 * 1100)
 #define GROUP_TABLE_LENGTH MAX_GROUPS
+#define INITIAL_NODE_LENGTH (1024 * 1024 * 21)
+
+/* Must be powers of two. */
 #define INITIAL_NODE_TABLE_LENGTH (1024 * 1024 * 32)
+#define STRING_STORAGE_TABLE_LENGTH (1024 * 1024 * 64)
+
 
 /* If you have a Linux with O_STREAMING, use the following define. */
 #define O_STREAMING    04000000
