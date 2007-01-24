74e8262f16f967445360b04839bdf8aab628ea1e
larsi <larsi>
2007-01-24 01:34:47 +0000

    Fix int/unsigned int.

--- hash.h
+++ hash.h
@@ -7,7 +7,7 @@ unsigned int enter_string_storage(const char *string);
 void flush_hash(void);
 group *get_group(const char *group_name);
 node *get_node(const char *message_id, unsigned int group_id);
-char *get_string(int offset);
+char *get_string(unsigned int offset);
 unsigned int previous_instance_node;
 void flush_strings(void);
 group *find_group(const char *group_name);
